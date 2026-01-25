dbab6f605407d63ae7e9c1a4937eef7bfa4b84d1
Pallavi <pallavishimpale@gmail.com>
2026-01-26 01:56:45 +0530

    django authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
@@ -33,12 +33,12 @@ class MyAccountManager(BaseUserManager):
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
-        user.is_superadmin = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
 
-class Account(AbstractBaseUser):
+class Account(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
@@ -50,11 +50,11 @@ class Account(AbstractBaseUser):
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=False)
+    # is_superadmin = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = [ 'first_name', 'last_name']
 
     objects = MyAccountManager()
 
@@ -62,7 +62,7 @@ class Account(AbstractBaseUser):
         return self.email
     
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return self.is_superuser
     
     def has_module_perms(self, add_label):
         return True
